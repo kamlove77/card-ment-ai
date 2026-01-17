44870727c419325b5d70d179124c53cd877e9aa1
kamlove77 <kamlove77@gmail.com>
2026-01-17 21:13:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("card-ment-v1").then((cache) =>
+      cache.addAll(["./", "./index.html", "./app.js"])
+    )
+  );
+});
 
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});
