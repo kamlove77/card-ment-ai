deee694062dbb93be2677f15e66545cf9301b926
kamlove77 <kamlove77@gmail.com>
2026-01-18 13:06:44 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("card-ment-v1").then((cache) =>
-      cache.addAll(["./", "./index.html", "./app.js"])
-    )
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((resp) => resp || fetch(e.request))
-  );
-});
+self.addEventListener("fetch", () => {});
